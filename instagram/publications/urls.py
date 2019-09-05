4cfb87b39994a77128d1a484a4bf2366f78f035a
wwhite_darkness <olimpsdu@gmail.com>
2019-09-05 11:58:40 +0600

    Реализовано удаление поста

--- instagram/publications/urls.py
+++ instagram/publications/urls.py
@@ -5,4 +5,5 @@ app_name = 'publications'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('<int:post_pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
--- instagram/publications/views/post_views.py
+++ instagram/publications/views/post_views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import HttpResponse
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import *
 
 # from instagram.users.models import *
@@ -15,4 +18,17 @@ class PostListView(ListView):
     # def get_context_data(self, *, object_list=None, **kwargs):
     #     kwargs['recommended_users'] = User.objects.random()[:3]
     #     return super().get_context_data(**kwargs)
+
+
+class PostDeleteView(UserPassesTestMixin, View):
+    def get(self, request, *args, **kwargs):
+        post_id = kwargs.get('post_pk')
+        post = get_object_or_404(Post, pk=post_id)
+        post.delete()
+        return redirect('publications:post_list')
+
+    def test_func(self):
+        post_id = self.kwargs.get('post_pk')
+        post = get_object_or_404(Post, pk=post_id)
+        return self.request.user.pk == post.author.pk
 
