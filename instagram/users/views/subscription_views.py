eeac5f87213ad79c866290fbf4d9316589e6a2a6
wwhite_darkness <olimpsdu@gmail.com>
2019-09-07 17:27:24 +0600

    Добавлен UserSubscribeView

---
+++ instagram/users/views/subscription_views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import *
+
+from ..models import *
+
+
+class UserSubscribeView(View):
+    def get(self, request, *args, **kwargs):
+        user = get_object_or_404(User, pk=kwargs.get('user_pk'))
+        response = {}
+
+        try:
+            subscription = Subscription.objects.get(subscriber=self.request.user, subscribed_to=user)
+            subscription.delete()
+            response['is_subscribed'] = False
+        except Subscription.DoesNotExist:
+            Subscription.objects.create(subscriber=self.request.user, subscribed_to=user)
+            response['is_subscribed'] = True
+
+        return JsonResponse(response['is_subscribed'])
