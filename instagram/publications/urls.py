dd2de8530818e4a502a240cfa518b9aeba29fbcf
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-11-20 16:55:41 +0600

    Добавлен префикс posts/ в publications/urls.py

--- instagram/publications/urls.py
+++ instagram/publications/urls.py
@@ -5,11 +5,11 @@ app_name = 'publications'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-    path('<int:post_pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('<int:post_pk>/edit/', PostEditView.as_view(), name='post_edit'),
-    path('<int:post_pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('<int:post_pk>/comment/', CommentCreateView.as_view(), name='comment_create'),
-    path('<int:post_pk>/like/', LikeView.as_view(), name='like'),
-    path('new_post/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:post_pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('posts/<int:post_pk>/edit/', PostEditView.as_view(), name='post_edit'),
+    path('posts/<int:post_pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:post_pk>/comment/', CommentCreateView.as_view(), name='comment_create'),
+    path('posts/<int:post_pk>/like/', LikeView.as_view(), name='like'),
+    path('posts/new_post/', PostCreateView.as_view(), name='post_create'),
     path('comments/<int:comment_pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
