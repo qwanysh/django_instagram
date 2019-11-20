8660074c165d51ab11b687d129b62f017a148ccc
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-11-20 17:03:15 +0600

    Изменен url для publications:post_create

--- instagram/publications/urls.py
+++ instagram/publications/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('posts/<int:post_pk>/', PostDetailView.as_view(), name='post_detail'),
     path('posts/<int:post_pk>/comment/', CommentCreateView.as_view(), name='comment_create'),
     path('posts/<int:post_pk>/like/', LikeView.as_view(), name='like'),
-    path('posts/new_post/', PostCreateView.as_view(), name='post_create'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
     path('comments/<int:comment_pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
