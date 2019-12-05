29fe0e10fd137d9ccea7a34bb069811636d2c805
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-05 12:14:33 +0600

    Настроен файл fabfile.py с командой deploy

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+import os
+from fabric import task
+
+PROJECT_DIR = '/home/projects/django_instagram'
+VIRTUALENV_ACTIVATE_CMD = 'source /home/projects/django_instagram/venv/bin/activate'
+USERNAME = 'root'
+# HOSTNAME = os.environ.get('host')
+HOSTNAME = '157.230.99.70'
+hosts = [f'{USERNAME}@{HOSTNAME}']
+
+
+@task(hosts=hosts)
+def deploy(connection):
+    connection.run(f'cd {PROJECT_DIR}; git pull')
+    connection.run(f'{VIRTUALENV_ACTIVATE_CMD}; cd {PROJECT_DIR}; pip3 install -r requirements.txt')
+    connection.run(f'{VIRTUALENV_ACTIVATE_CMD}; cd {PROJECT_DIR}/instagram; python3 manage.py collectstatic --noinput')
