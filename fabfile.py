59225e520b5c11df9e3dfdf148b8e1235327ccc7
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-05 12:41:15 +0600

    В fabfile.py добавлена команда supervisorctl restart all

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,8 @@
-import os
 from fabric import task
 
 PROJECT_DIR = '/home/projects/django_instagram'
 VIRTUALENV_ACTIVATE_CMD = 'source /home/projects/django_instagram/venv/bin/activate'
 USERNAME = 'root'
-# HOSTNAME = os.environ.get('host')
 HOSTNAME = '157.230.99.70'
 hosts = [f'{USERNAME}@{HOSTNAME}']
 
@@ -14,3 +12,4 @@ def deploy(connection):
     connection.run(f'cd {PROJECT_DIR}; git pull')
     connection.run(f'{VIRTUALENV_ACTIVATE_CMD}; cd {PROJECT_DIR}; pip3 install -r requirements.txt')
     connection.run(f'{VIRTUALENV_ACTIVATE_CMD}; cd {PROJECT_DIR}/instagram; python3 manage.py collectstatic --noinput')
+    connection.run('supervisorctl restart all')
